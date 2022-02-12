fb87a2953ff7aef80f01ffbacb537f6f97b91d69
Oscar Rodenas <rodenas.oscar@gmail.com>
2022-02-12 10:21:42 +0100

    Spring security

--- src/main/java/com/example/Sesiones456/controllers/HelloController.java
+++ src/main/java/com/example/Sesiones456/controllers/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.Sesiones456.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${app.varexample}")
+    String message;
+
+
+
     @GetMapping("/hellocontroller")
     public String HelloController() {
-        return ("Buenos días");
+        System.out.println(message);
+        return ("Buenos días, " + message);
     }
 }
